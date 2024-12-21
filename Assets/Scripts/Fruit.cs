694abddb7cac102f4c4ef47833135f711e21eaa6
easy-jo-cloud <bluecarrot2box@gmail.com>
2024-12-22 06:19:49 +0900

    Initial commit

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public float timeAdd = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            GameManager.Instance.AddTime(timeAdd);
+            GetComponent<Animator>().SetTrigger("Eaten");
+            Invoke("DestroyThis", 0.3f);
+        }
+    }
+
+
+    void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+
+}
+
+
+
